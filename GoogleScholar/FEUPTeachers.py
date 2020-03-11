31586b5c822f2a1d9d450a5a0e6969ac091474a8
FilipePintoReis <filipepintodosreis@gmail.com>
2020-03-11 22:05:48 +0000

    prototype for teacher iteration

---
+++ GoogleScholar/FEUPTeachers.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+
+'''
+gs_btnPL 
+gs_btnPR
+
+initial_url = 'https://scholar.google.com/citations?view_op=view_org&hl=en&org=6139287000594560576'
+
+browser = webdriver.Firefox()
+browser.get(initial_url)
+
+browser.find_elements_by_class_name('gsc_1usr')
+for element in elements:
+    if element.contents[0].contents[1].contens[2].split()[3] == 'fe.up.pt':
+        element.contents[0].contents[1].contents[0].contens[0]
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+'''
