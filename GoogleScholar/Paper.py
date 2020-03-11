b8617167d0a79a77c302b061968e2d950c77f4bb
FilipePintoReis <filipepintodosreis@gmail.com>
2020-03-11 22:02:46 +0000

    Paper class

---
+++ GoogleScholar/Paper.py
@@ -0,0 +1,6 @@
+class Paper:
+    def __init__(self, name, authors, publication_date, total_citations):
+        self.name = name
+        self.authors = authors
+        self.publication_date = publication_date
+        self.total_citations = total_citations
