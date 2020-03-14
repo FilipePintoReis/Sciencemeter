7c56ca8c549db00560215a99e9e156fe3336e1a6
FilipePintoReis <filipepintodosreis@gmail.com>
2020-03-14 22:57:14 +0000

    Finished authenticus

--- Authenticus/Authenticus.py
+++ Authenticus/Authenticus.py
@@ -2,11 +2,12 @@ from bs4 import BeautifulSoup
 import time
 import random
 from selenium import webdriver
+import requests
 
-# import ParsePaper
+from Paper import Paper
 
 
-def parseProfile(url):
+def get_paper_links(url):
     browser = webdriver.Firefox()
     browser.get(url)
 
@@ -57,14 +58,62 @@ def parseProfile(url):
             if not flag:
                 browser.find_element_by_id(aid).click()
             else:
-                browser.close()
+                break
 
     except Exception as err:
-        print(len(links))
         print(f'Error occurred: {err}')
         time.sleep(3)
         browser.close()
 
+    browser.close()
+    return links
 
-parseProfile(
-    "https://www.authenticus.pt/en/profileOfResearchers/publicationsList/16105?total_results=162&page=1&_=1")
+
+def parse_page(link):
+    try:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Get title
+        title = soup.find(
+            class_='content-header').find('h4').contents[0].strip()
+
+        # Get publication date
+        meta = soup.find(class_="mainmeta").contents
+        while meta.count('\n') > 0:
+            meta.remove('\n')
+
+        publication_date = meta[1].find(
+            class_="authors-flex").contents[0].strip()
+
+        # Get authors
+        authors = []
+        authors_html = soup.find(
+            class_='authors-flex').find(class_='authors-flex').contents
+
+        for author_html in authors_html:
+            if author_html == ' ' or author_html == '\n':
+                authors_html.remove(author_html)
+
+        for author_html in authors_html:
+            if author_html.contents[0].contents[0].find('<a') == -1:
+                authors.append(author_html.contents[0].contents[0])
+            else:
+                authors.append(author_html.contents[0].contents[0]['title'])
+
+        return Paper(title, authors, publication_date)
+
+    except Exception as err:
+        print(f'Error occurred: {err}')
+
+
+def parse_profile(url):
+    links = get_paper_links(url)
+    papers = []
+    for link in links:
+        papers.append(parse_page(link))
+
+    return papers
+
+
+print(parse_profile("https://www.authenticus.pt/en/profileOfResearchers/publicationsList/16105?total_results=162&page=1&_=1"))
